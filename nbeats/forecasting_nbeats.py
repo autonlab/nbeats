5f851cbfb1bd1bf249535ed578a432b93fe086b3
Jarod <jarod.d.wang@gmail.com>
2020-10-13 13:42:33 -0400

    add metadata

--- nbeats/forecasting_nbeats.py
+++ nbeats/forecasting_nbeats.py
@@ -1,3 +1,4 @@
+import os
 from typing import Any
 from d3m import container, utils as d3m_utils
 from d3m.exceptions import PrimitiveNotFittedError
@@ -5,6 +6,7 @@ from d3m.metadata import params
 from d3m.metadata import base as metadata_base, hyperparams
 from d3m.primitive_interfaces.base import Params, CallResult
 from d3m.primitive_interfaces.supervised_learning import SupervisedLearnerPrimitiveBase
+import nbeats
 
 Inputs = container.DataFrame
 Outputs = container.DataFrame
@@ -30,8 +32,37 @@ class ForecastingNBEATSHyperparams(hyperparams.Hyperparams):
 class ForecastingNBEATSPrimitive(SupervisedLearnerPrimitiveBase[Inputs, Outputs, ForecastingNBEATSParams,
                                                                 ForecastingNBEATSHyperparams]):
     """
-    Hybrid ES-RNN models for time series forecasting
+    N-BEATS for time series forecasting
     """
+    metadata = metadata_base.PrimitiveMetadata(
+        {
+            'id': 'bd925663-aeaf-4240-9748-cd77dce33819',
+            'version': '0.1.0',
+            "name": "N-BEATS models for time series forecasting",
+            'description': "Pytorch Implementation of N-BEATS. The model is doing local projections to basis "
+                           "functions. These functions include \"trends\" (with polynomial functions) and "
+                           "\"seasonalities\" (with harmonic functions). The prediction will consist of adding the "
+                           "local projections to these basis functions to the last available value in the ts (Naive "
+                           "1). The model decomposes the signals successively through different \"blocks\" of a fully "
+                           "connected residual NN.",
+            'python_path': 'd3m.primitives.time_series_forecasting.nbeats.DeepNeuralNetwork',
+            'source': {
+                'name': nbeats.__author__,
+                'uris': ['https://github.com/autonlab/nbeats'],
+                'contact': 'mailto:donghanw@cs.cmu.edu'
+            },
+            'installation': [{
+                'type': metadata_base.PrimitiveInstallationType.PIP,
+                'package_uri': 'git+https://github.com/autonlab/nbeats.git@{git_commit}#egg=nbeats'.format(
+                    git_commit=d3m_utils.current_git_commit(os.path.dirname(__file__)),
+                ),
+            }],
+            'algorithm_types': [
+                metadata_base.PrimitiveAlgorithmType.DEEP_NEURAL_NETWORK,
+            ],
+            'primitive_family': metadata_base.PrimitiveFamily.TIME_SERIES_FORECASTING,
+        },
+    )
 
     def produce(self, *, inputs: Inputs, timeout: float = None, iterations: int = None) -> CallResult[Outputs]:
         pass
